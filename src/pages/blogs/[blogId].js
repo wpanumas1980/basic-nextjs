6426b3081cf16b58b1ad00f814c852afbc1fef7f
panumas <wpanumas@gmail.com>
2021-09-19 08:46:16 +0700

    fallback getStaticPaths

--- src/pages/blogs/[blogId].js
+++ src/pages/blogs/[blogId].js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { getMovies } from '../../db';
+import { useRouter } from 'next/router';
 
 export default function Blog({ movie }) {
+  const router = useRouter();
   return (
     <div style={{ width: '80%', margin: '0 auto' }}>
       {/* details form database */}
-      <h2>{movie.title}</h2>
-      <span>
-        {movie.writer} / {movie.published_date}
-      </span>
-      <p>{movie.detail}</p>
+      {router.isFallback ?
+        <p>Loading...</p>
+        :
+        <>
+          <h2>{movie.title}</h2>
+          <span>
+            {movie.writer} / {movie.published_date}
+          </span>
+          <p>{movie.detail}</p>
+        </>
+      }
     </div>
   )
 };
@@ -20,12 +28,12 @@ export const getStaticPaths = async () => {
   const paths = movies.map(movie => ({ params: { blogId: movie.id } }));
 
   return {
-    paths,
-    fallback: false
+    paths: paths.slice(0, 5),
+    fallback: true
   }
 };
 
-export const getStaticProps = async ({params}) => {
+export const getStaticProps = async ({ params }) => {
 
   const movies = await getMovies();
   const movie = movies.find(movie => movie.id === params.blogId);
